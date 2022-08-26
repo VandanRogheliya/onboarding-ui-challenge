d928ddb376bab4951872ecab7dee6734cc874b32
Vandan Rogheliya <rogheliavandan@outlook.com>
2022-08-26 20:54:22 +0530

    remove console log

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,6 @@ const TOTAL_STEPS = 4
 const Home: NextPage = () => {
 	const [activeStep, setActiveStep] = useState(STEP_ENUM.welcome)
 
-	useEffect(() => {
-		console.log(activeStep)
-	}, [activeStep])
-
 	return (
 		<div className="flex justify-center">
 			<div className="flex flex-col items-center max-w-4xl px-5 py-10 h-screen space-y-10">
